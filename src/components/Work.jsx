9d8df667d770f11daf83081e283b794d4278f1d7
IndiraPriyadharshini-63 <indirapriyadharshini.mary@gmail.com>
2024-06-23 15:42:26 +0530

    image alt changes

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -8,6 +8,28 @@ function Work() {
           <img
             src="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
             class="w-full mb-3"
+            alt="project"
+          />
+          <div class="p-4 pt-2">
+            <div class="mb-8">
+              <a
+                href="/chat"
+                class="text-[#b70048] font-bold text-lg mb-2 hover:text-indigo-600 inline-block"
+              >
+                Chat Application
+              </a>
+              <p class="text-[#b70048] text-sm">
+                Chat Application built with react and mongoDB and Node JS
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div class=" shadow-xl shadow-[#ffc6e9] hover:shadow-xl hover:shadow-[#ff98d5] border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal  group transition ">
+          <img
+            src="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
+            class="w-full mb-3"
+            alt="project"
           />
           <div class="p-4 pt-2">
             <div class="mb-8">
@@ -28,6 +50,7 @@ function Work() {
           <img
             src="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
             class="w-full mb-3"
+            alt="project"
           />
           <div class="p-4 pt-2">
             <div class="mb-8">
@@ -48,6 +71,7 @@ function Work() {
           <img
             src="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
             class="w-full mb-3"
+            alt="project"
           />
           <div class="p-4 pt-2">
             <div class="mb-8">
@@ -68,6 +92,7 @@ function Work() {
           <img
             src="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
             class="w-full mb-3"
+            alt="project"
           />
           <div class="p-4 pt-2">
             <div class="mb-8">
@@ -88,26 +113,7 @@ function Work() {
           <img
             src="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
             class="w-full mb-3"
-          />
-          <div class="p-4 pt-2">
-            <div class="mb-8">
-              <a
-                href="/chat"
-                class="text-[#b70048] font-bold text-lg mb-2 hover:text-indigo-600 inline-block"
-              >
-                Chat Application
-              </a>
-              <p class="text-[#b70048] text-sm">
-                Chat Application built with react and mongoDB
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div class=" shadow-xl shadow-[#ffc6e9] hover:shadow-xl hover:shadow-[#ff98d5] border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal  group transition ">
-          <img
-            src="https://images.pexels.com/photos/1181467/pexels-photo-1181467.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=1&amp;w=500"
-            class="w-full mb-3"
+            alt="project"
           />
           <div class="p-4 pt-2">
             <div class="mb-8">
