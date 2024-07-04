d892e298b7ae654d2342878454bae426a5d4d48d
IndiraPriyadharshini-63 <indirapriyadharshini.mary@gmail.com>
2024-07-04 12:31:14 +0530

    new work added

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import WorkList from "./WorkList";
 import taskImage from "../assets/task.png";
-
+import sortImage from "../assets/sorting.png";
 
 function Work() {
   // eslint-disable-next-line
@@ -13,6 +13,13 @@ function Work() {
       description: "Web App which helps to create and manage daily tasks ",
       code_link: "https://github.com/IndiraPriyadharshini-63/REACT-CRUD",
     },
+    {
+    id: 1,
+    image: sortImage,
+    title: "Sorting Visualizer",
+    description: "Web application using React JS to visualize how various sorting algorithms work",
+    code_link: "https://github.com/IndiraPriyadharshini-63/SortingVisualizer",
+  }
   ]);
   return (
     <div name="work" className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
